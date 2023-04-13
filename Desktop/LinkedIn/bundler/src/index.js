2f6cc0d5f25369e390e8d13301618d6dc79b468a
Fiora De Marco <fiorademarco7@gmail.com>
2023-04-13 14:35:40 -0400

    added loaders

--- Desktop/LinkedIn/bundler/src/index.js
+++ Desktop/LinkedIn/bundler/src/index.js
@@ -1,3 +1,14 @@
+import './style.scss'
 import {camelCase} from 'lodash';
 
 console.log(camelCase('hello world'))
+
+
+
+
+//In Webpack a loader is a way to preprocess a file. In this case we want to import some css code and then
+//tell our javascript to inject that css in the DOM when our web application is loaded. 
+
+//  Essentially, loaders provide a way to handle any kind of file that's not plain JavaScript
+
+
